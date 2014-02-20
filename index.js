1480a8b2233a80af838efa57bde5035c766d5b74
deathcap <deathcap@gmx.co.uk>
2014-02-19 20:16:17 -0800

    Heal player (with voxel-health) on consuming food (voxel-use)
    Requires:
    deathcap/voxel-use@2e09a12812e89cdbba8d856a40bffb9c4aab22ba
    deathcap/voxel-health@5e40ebdaefa88f333e7fd17aeb43ea53add43c9e

--- index.js
+++ index.js
@@ -10,16 +10,33 @@ module.exports.pluginInfo = {
 
 function FoodPlugin(game, opts) {
   this.registry = game.plugins.get('voxel-registry');
-  if (!this.registry) throw new Error('voxel-food requires voxel-registry');
+  if (!this.registry) throw new Error('voxel-food requires voxel-registry plugin');
+
+  this.health = game.plugins.get('voxel-health');
+  if (!this.health) throw new Error('voxel-food requires voxel-health plugin');
 
   this.enable();
 };
 
 FoodPlugin.prototype.enable = function() {
-  this.registry.registerItem('cookie', {itemTexture: 'i/cookie'});
+  this.registry.registerItem('cookie', {itemTexture: 'i/cookie', onUse: this.eat.bind(this)});
 };
 
 FoodPlugin.prototype.disable = function() {
   // TODO: unregister items
 };
 
+FoodPlugin.prototype.eat = function(item) {
+  var amount = 1; // TODO: per item
+
+  var effectiveAmount = this.health.heal(amount);
+
+  if (effectiveAmount === 0) {
+    return false; // didn't eat anything, we're full
+  } else {
+
+    // TODO: emit eat event for voxel-sfx
+
+    return true; // ate something, consume item
+  }
+};
