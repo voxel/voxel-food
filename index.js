d7235c8a29a99e8ffda7ef8adc6c17aa9ceb1583
deathcap <deathcap@gmx.co.uk>
2014-02-20 17:52:03 -0800

    Disable most food items by default for now to avoid clutter, may enable again later. Ref GH-1

--- index.js
+++ index.js
@@ -16,20 +16,21 @@ function FoodPlugin(game, opts) {
   if (!this.health) throw new Error('voxel-food requires voxel-health plugin');
 
   this.items = opts.items || {
+    // TODO: rebalance/re-enable
     apple: {itemTexture: 'i/apple', foodAmount: 2.0},
-    appleGolden: {itemTexture: 'i/apple_golden', foodAmount: 999.0},
-    beefCooked: {itemTexture: 'i/beef_cooked', foodAmount: 2.0},
-    beefRaw: {itemTexture: 'i/beef_raw', foodAmount: 0.1},
+    //appleGolden: {itemTexture: 'i/apple_golden', foodAmount: 999.0},
+    //beefCooked: {itemTexture: 'i/beef_cooked', foodAmount: 2.0},
+    //beefRaw: {itemTexture: 'i/beef_raw', foodAmount: 0.1},
     bread: {itemTexture: 'i/bread', foodAmount: 3.0},
-    cake: {itemTexture: 'i/cake', foodAMount: 3.5},
-    carrot: {itemTexture: 'i/carrot', foodAmount: 0.25},
-    carrotGolden: {itemTexture: 'i/carrot_golden', foodAmount: 5.0},
-    chickenCooked: {itemTexture: 'i/chicken_cooked', foodAmount: 2.0},
-    chickenRaw: {itemTexture: 'i/chicken_raw', foodAmount: 0.1},
-    cookie: {itemTexture: 'i/cookie', foodAmount: 0.75},
-    melon: {itemTexture: 'i/melon', foodAmount: 0.5},
-    potatoBaked: {itemTexture: 'i/potato', foodAmount: 0.75},
-    pumpkinPie: {itemTexture: 'i/pumpkin_pie', foodAmount: 7.0},
+    //cake: {itemTexture: 'i/cake', foodAMount: 3.5},
+    //carrot: {itemTexture: 'i/carrot', foodAmount: 0.25},
+    //carrotGolden: {itemTexture: 'i/carrot_golden', foodAmount: 5.0},
+    //chickenCooked: {itemTexture: 'i/chicken_cooked', foodAmount: 2.0},
+    //chickenRaw: {itemTexture: 'i/chicken_raw', foodAmount: 0.1},
+    cookie: {itemTexture: 'i/cookie', foodAmount: 0.5},
+    //melon: {itemTexture: 'i/melon', foodAmount: 0.5},
+    //potatoBaked: {itemTexture: 'i/potato_baked', foodAmount: 0.75},
+    //pumpkinPie: {itemTexture: 'i/pumpkin_pie', foodAmount: 7.0},
     spiderEye: {itemTexture: 'i/spider_eye', foodAmount: -1.0},
   };
 
