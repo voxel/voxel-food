02eac8bfe64b86e7fe288aa82bd809af55e8ac2b
deathcap <deathcap@gmx.co.uk>
2014-05-02 18:36:10 -0700

    Re-enable all food types
    Previously only a select few were enabled by default since the creative
    inventory was getting crowded, but now that it is tabbed
    https://github.com/deathcap/voxel-inventory-creative space is less of a
    problem so just enable everything.

--- food.js
+++ food.js
@@ -16,21 +16,21 @@ function FoodPlugin(game, opts) {
   if (!this.health) throw new Error('voxel-food requires voxel-health plugin');
 
   this.items = opts.items || {
-    // TODO: rebalance/re-enable
+    // TODO: rebalance
     apple: {itemTexture: 'i/apple', foodAmount: 2.0, creativeTab: 'food'},
-    //appleGolden: {itemTexture: 'i/apple_golden', foodAmount: 999.0, creativeTab: 'food'},
-    //beefCooked: {itemTexture: 'i/beef_cooked', foodAmount: 2.0, creativeTab: 'food'},
-    //beefRaw: {itemTexture: 'i/beef_raw', foodAmount: 0.1, creativeTab: 'food'},
+    appleGolden: {itemTexture: 'i/apple_golden', foodAmount: 999.0, creativeTab: 'food'},
+    beefCooked: {itemTexture: 'i/beef_cooked', foodAmount: 2.0, creativeTab: 'food'},
+    beefRaw: {itemTexture: 'i/beef_raw', foodAmount: 0.1, creativeTab: 'food'},
     bread: {itemTexture: 'i/bread', foodAmount: 3.0, creativeTab: 'food'},
-    //cake: {itemTexture: 'i/cake', foodAMount: 3.5, creativeTab: 'food'},
-    //carrot: {itemTexture: 'i/carrot', foodAmount: 0.25, creativeTab: 'food'},
-    //carrotGolden: {itemTexture: 'i/carrot_golden', foodAmount: 5.0, creativeTab: 'food'},
-    //chickenCooked: {itemTexture: 'i/chicken_cooked', foodAmount: 2.0, creativeTab: 'food'},
-    //chickenRaw: {itemTexture: 'i/chicken_raw', foodAmount: 0.1, creativeTab: 'food'},
+    cake: {itemTexture: 'i/cake', foodAMount: 3.5, creativeTab: 'food'},
+    carrot: {itemTexture: 'i/carrot', foodAmount: 0.25, creativeTab: 'food'},
+    carrotGolden: {itemTexture: 'i/carrot_golden', foodAmount: 5.0, creativeTab: 'food'},
+    chickenCooked: {itemTexture: 'i/chicken_cooked', foodAmount: 2.0, creativeTab: 'food'},
+    chickenRaw: {itemTexture: 'i/chicken_raw', foodAmount: 0.1, creativeTab: 'food'},
     cookie: {itemTexture: 'i/cookie', foodAmount: 0.5, creativeTab: 'food'},
-    //melon: {itemTexture: 'i/melon', foodAmount: 0.5, creativeTab: 'food'},
-    //potatoBaked: {itemTexture: 'i/potato_baked', foodAmount: 0.75, creativeTab: 'food'},
-    //pumpkinPie: {itemTexture: 'i/pumpkin_pie', foodAmount: 7.0, creativeTab: 'food'},
+    melon: {itemTexture: 'i/melon', foodAmount: 0.5, creativeTab: 'food'},
+    potatoBaked: {itemTexture: 'i/potato_baked', foodAmount: 0.75, creativeTab: 'food'},
+    pumpkinPie: {itemTexture: 'i/pumpkin_pie', foodAmount: 7.0, creativeTab: 'food'},
     spiderEye: {itemTexture: 'i/spider_eye', foodAmount: -1.0, creativeTab: 'food'},
   };
 
